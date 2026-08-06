eea3b87ea1edbce4e8aaaffaee3dd4ef691dfe23
javallecillo <javallecillo@uth.hn>
2026-08-06 02:13:39 -0600

    citaconcomentario

--- odontogest_web/odontogest_api/agenda/crear.php
+++ odontogest_web/odontogest_api/agenda/crear.php
@@ -28,6 +28,7 @@ if (!preg_match('/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}/', $fecha)) {
 }
 
 $paso = 'conectar a base de datos';
+//comentario
 try {
     $db = getDB();
     $paso = 'validar disponibilidad';
